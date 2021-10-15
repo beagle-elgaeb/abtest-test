5a45721cecb6afdf40acb95b4e60e1eef7a818c3
Евгения Никонова <begale-dog@mail.ru>
2021-10-15 13:54:19 +0300

    Улучшено отображение гистограммы

--- web/src/components/Metric.tsx
+++ web/src/components/Metric.tsx
@@ -1,3 +1,4 @@
+import { ChartOptions } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import styled from "styled-components/macro";
 import { UserData } from "../utils/types";
@@ -7,30 +8,53 @@ interface Props {
   users: UserData[];
 }
 
-// лучше округлять. например, до большего
-// и разместить столбцы гистограммы между интервалами,
-// но уже пора спать
-
 export function Metric(props: Props) {
   const intervals = calcIntervals(props.users);
 
   const labels = intervals.map((interval, i) => {
-    if (i <= 1) {
-      return `${i} week`;
-    }
-    return `${i} weeks`;
+    return `${i}`;
   });
 
   const data = {
     labels: labels,
     datasets: [
       {
+        label: "Quantity of users",
         data: intervals,
-        backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-        borderColor: ["rgba(54, 162, 235, 1)"],
+        backgroundColor: ["rgba(74, 157, 255, 0.2)"],
+        borderColor: ["rgba(74, 157, 255, 0.8)"],
         borderWidth: 1,
       },
     ],
+  };
+
+  const options: ChartOptions = {
+    plugins: {
+      legend: { display: false },
+    },
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: "Quantity of users",
+          color: "#4A9DFF",
+        },
+        ticks: {
+          stepSize: 1,
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Weeks",
+          color: "#4A9DFF",
+        },
+        ticks: {
+          labelOffset: -30, // пока не разобралась, как нормально сместить значения под точку, а не под столбик
+          // это решение хорошо выглялит на 12 неделях
+        },
+      },
+    },
   };
 
   if (props.rollingRetention === undefined) {
@@ -38,7 +62,7 @@ export function Metric(props: Props) {
   }
 
   return (
-    <MetricContainer>
+    <div className="MetricContainer">
       <MetricTitle>
         Rolling Retention 7 day:{" "}
         {Number.isNaN(props.rollingRetention)
@@ -48,18 +72,12 @@ export function Metric(props: Props) {
       <Histogram>
         {props.users && <Bar data={data} options={options} />}
       </Histogram>
-    </MetricContainer>
+    </div>
   );
 }
 
-const options = {
-  plugins: {
-    legend: { display: false },
-  },
-};
-
-// todo: возможно добавить агрумент длительности интервалов
-// todo: возможно добавить выбор интервала, для которого строится график
+// todo: возможно добавить выбор интервалов
+// todo: возможно добавить выбор диапазона, для которого строится график
 function calcIntervals(users: UserData[]) {
   let groups: number[] = [];
 
@@ -70,7 +88,7 @@ function calcIntervals(users: UserData[]) {
       const reg = new Date(user.dateRegistration);
       const lastAct = new Date(user.dateLastActivity);
 
-      const period = Math.round((lastAct.getTime() - reg.getTime()) / msInWeek);
+      const period = Math.floor((lastAct.getTime() - reg.getTime()) / msInWeek);
 
       if (!groups[period]) {
         groups[period] = 0;
@@ -81,8 +99,6 @@ function calcIntervals(users: UserData[]) {
 
   return Array.from(groups); //убирает пропуски в массиве, чтоб не лишать график пропорциональности
 }
-
-const MetricContainer = styled.div``;
 
 const MetricTitle = styled.h3`
   font-size: 17px;
